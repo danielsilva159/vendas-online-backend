5025f885eb14413798073de3fc3c69dc64b77b06
Daniel Ferreira <silva_120000@yahoo.com.br>
2024-11-12 06:02:14 -0300

    chore: adding cache

---
+++ src/city/city.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { CityEntity } from './entities/city.entity';
+import { CityService } from './city.service';
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+
+@Controller('city')
+export class CityController {
+  constructor(
+    private readonly cityService: CityService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
+
+  @Get('/:stateId')
+  async getAllCitiesByStateId(
+    @Param('stateId') stateId: number,
+  ): Promise<CityEntity[]> {
+    return this.cityService.getAllCitiesByStateId(stateId);
+  }
+}
